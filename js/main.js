7449805216489e3857d80333c5c1390282865a72
Dmitriy Kurchenko <dmitriy1991dp@gmail.com>
2020-11-23 22:33:45 +0200

    got more detailed information about countries and did rendering data to display on a modal window

--- js/main.js
+++ js/main.js
@@ -5,7 +5,9 @@ const runScript = () => {
    const countryCardsContainer = document.querySelector(".countries__body"),
       searchForm = document.querySelector(".search-form"),
       searchInput = document.querySelector(".search-form__input-search"),
-      regionFilters = document.querySelectorAll(".dropdown__link");
+      regionFilters = document.querySelectorAll(".dropdown__link"),
+      btnClose = document.querySelector(".country-modal__close"),
+      modal = document.querySelector(".country-modal");
 
 
    // Show and hide dropdown menu onclick
@@ -35,7 +37,20 @@ const runScript = () => {
       })
    };
 
-   fetch("https://restcountries.eu/rest/v2/name/mexico")
+   // close a modal window on click
+   const closeModal = () => {
+      modal.classList.toggle("show");
+   };
+
+   //close a modal window by clicking outside it
+   const closeModalOverlay = (event) => {
+      const target = event.target;
+      if (!target.classList.contains("country-modal__body")) {
+         closeModal();
+      }
+   };
+
+   fetch("https://restcountries.eu/rest/v2/name/afganistan")
       .then(response => response.json())
       .then(data => console.log(data[0]));
 
@@ -51,12 +66,13 @@ const runScript = () => {
    // render country cards based on recived data from API
    const renderCountryCard = (countries) => {
       countries.forEach(country => { //iterate over recived countries data to render a specific card with info
+     
+         const countryDetails= document.createElement("a");
+         countryDetails.classList.add("countries__card");
 
-         const countryDeatils = `
-         <!--Card starts-->
-              <a class="countries__card">
+          countryDetails.innerHTML = `
                   <figure class="countries__flag">
-                     <img src="${country.flag}" alt="country flag" class="countries__img">
+                     <img src="${country.flag}" alt="country flag" class="countries__img" loading="lazy">
                   </figure>
                   <div class="countries__country-info">
                      <h3 class="countries__title">${country.name}</h3>
@@ -69,11 +85,16 @@ const runScript = () => {
                               class="countries__country-feature--description">${country.capital}</span>
                      </p>
                   </div>
-              </a>
-            <!--Card ends-->
-      `
-         countryCardsContainer.insertAdjacentHTML("beforeend", countryDeatils)
-      })
+      `;
+         countryDetails.addEventListener("click", () => { // click on a coutry card
+            modal.classList.toggle("show");
+            displayCountryDetails(country);
+         });
+
+         countryCardsContainer.appendChild(countryDetails);
+         //countryCardsContainer.insertAdjacentHTML("beforeend", countryDetails);
+      });
+      
    };
 
    // display occured errors
@@ -83,6 +104,26 @@ const runScript = () => {
          countryCardsContainer.insertAdjacentHTML("beforeend", message);
          countryCardsContainer.classList.toggle("error");
       }
+   };
+
+   //display a modal window with a rendered details about a specific country
+   const displayCountryDetails = (country) => {
+      const modalBody = document.querySelector(".country-modal__info");
+      const countryFlag = document.querySelector(".country-modal__img");
+
+      countryFlag.src = country.flag;
+
+      modalBody.innerHTML = `
+            <h3 class="country-modal__name">${country.name}</h3>
+            <p class="country-modal__country-info">Native Name:<span class="country-modal__country-info--description">${country.nativeName}</span></p>
+            <p class="country-modal__country-info">Population:<span class="country-modal__country-info--description">${commaSeparation(country.population)}</span></p>
+            <p class="country-modal__country-info">Region:<span class="country-modal__country-info--description">${country.region}</span>
+            <p class="country-modal__country-info">Sub Region:<span class="country-modal__country-info--description">${country.subregion}</span></p>
+            <p class="country-modal__country-info">Capital:<span class="country-modal__country-info--description">${country.capital}</span></p>
+            <p class="country-modal__country-info">Timezones:<span class="country-modal__country-info--description">${country.timezones}</span></p>
+            <p class="country-modal__country-info">Currency:<span class="country-modal__country-info--description">${country.currencies.map(currency => currency.name)}</span></p>
+            <p class="country-modal__country-info">Languages:<span class="country-modal__country-info--description">${country.languages.map(language => language.name)}</span></p>
+      `;
    };
 
    // Search a specific country by typing it in the search input field
@@ -133,6 +174,8 @@ const runScript = () => {
    });
 
    searchInput.addEventListener("keyup", searchCountry);
+   btnClose.addEventListener("click", closeModal);
+   modal.addEventListener("click", closeModalOverlay)
 
 
    //call functions
